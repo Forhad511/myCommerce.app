69f0db9da3fa3032c83ca08093155308a621182c
MD FORHAD HOSSAIN <forhadpatwary342@gmail.com>
2023-07-19 08:27:37 +0600

    first commit myCommerce.app

---
+++ resources/views/website/includes/head.blade.php
@@ -0,0 +1,31 @@
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta content="Codescandy" name="author">
+<title>Homepage v1 - eCommerce HTML Template - FreshCart </title>
+
+<link href="{{asset('/')}}/website/assets/libs/slick-carousel/slick/slick.css" rel="stylesheet" />
+<link href="{{asset('/')}}/website/assets/libs/slick-carousel/slick/slick-theme.css" rel="stylesheet" />
+<link href="{{asset('/')}}/website/assets/libs/tiny-slider/dist/tiny-slider.css" rel="stylesheet">
+
+<!-- Favicon icon-->
+<link rel="shortcut icon" type="image/x-icon" href="{{asset('/')}}/website/assets/images/favicon/favicon.ico">
+
+
+<!-- Libs CSS -->
+<link href="{{asset('/')}}/website/assets/libs/bootstrap-icons/font/bootstrap-icons.min.css" rel="stylesheet">
+<link href="{{asset('/')}}/website/assets/libs/feather-webfont/dist/feather-icons.css" rel="stylesheet">
+<link href="{{asset('/')}}/website/assets/libs/simplebar/dist/simplebar.min.css" rel="stylesheet">
+
+
+<!-- Theme CSS -->
+<link rel="stylesheet" href="{{asset('/')}}/website/assets/css/theme.min.css">
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-M8S4MT3EYG"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-M8S4MT3EYG');
+</script>
---
+++ resources/views/website/includes/javascript.blade.php
@@ -0,0 +1,15 @@
+<!-- Libs JS -->
+<script src="{{asset('/')}}/website/assets/libs/jquery/dist/jquery.min.js"></script>
+<script src="{{asset('/')}}/website/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<script src="{{asset('/')}}/website/assets/libs/simplebar/dist/simplebar.min.js"></script>
+
+<!-- Theme JS -->
+<script src="{{asset('/')}}/website/assets/js/theme.min.js"></script>
+<script src="{{asset('/')}}/website/assets/libs/jquery-countdown/dist/jquery.countdown.min.js"></script>
+<script src="{{asset('/')}}/website/assets/js/vendors/countdown.js"></script>
+<script src="{{asset('/')}}/website/assets/libs/slick-carousel/slick/slick.min.js"></script>
+<script src="{{asset('/')}}/website/assets/js/vendors/slick-slider.js"></script>
+<script src="{{asset('/')}}/website/assets/libs/tiny-slider/dist/min/tiny-slider.js"></script>
+<script src="{{asset('/')}}/website/assets/js/vendors/tns-slider.js"></script>
+<script src="{{asset('/')}}/website/assets/js/vendors/zoom.js"></script>
+<script src="{{asset('/')}}/website/assets/js/vendors/increment-value.js"></script>
